392caef822ff3a2351e2e41b78a89667fc00d8b8
Crosson Brieuc <crosson.brieuc@gmail.com>
2022-12-07 11:25:48 +0100

    d6

---
+++ d6/script.js
@@ -0,0 +1,23 @@
+
+const test_sub = (l) => l
+	.every((x,i,l)=>
+		!(l.slice(0,i).includes(x)) && !(l.slice(i+1).includes(x))
+	);
+
+
+Array.prototype.first = function(test,len){
+	for(let i=0;i<this.length-len;i++){
+		if(test(this.slice(i, i+len))){
+			return i;
+		}
+	}
+	return -1;
+}
+
+
+exports.run = (input) => input
+	.split('')
+	.first(test_sub,4) + 4;
+
+
+
