c2c633f39afb24b2312de9bef4ec7c406856adfa
briossant <crosson.brieuc@gmail.com>
2022-12-10 12:09:11 +0100

    d9 pt2

--- d9/script2.js
+++ d9/script2.js
@@ -1,37 +1,58 @@
 
-Array.prototype.trans = function(){return this[0].map((x,i) => this.map(x => x[i]));}
-
-Array.prototype.count = function(x){
-    for(let i=0; i<this.length; i++){
-        if(this[i]>=x){
-            return i+1;
-        }
-    }
-    return this.length
-}
-
-
-
 const parse = (input) => input 
     .split('\n')
     .slice(0,-1)
     .map(x=>x
-        .split('')
-        .map(x=>parseInt(x))
+        .split(' ')
+        .map((x,i)=>i==1?parseInt(x):x)
     );
 
-const calculate = (grid, tgrid) => grid
-    .map((x,i) => x
-        .map((y,j) => x.slice(0,j).reverse().count(y)
-            * x.slice(j+1).count(y)
-            * tgrid[j].slice(0,i).reverse().count(y)
-            * tgrid[j].slice(i+1).count(y)
-        )
-    );
 
+const tocoo = (grid, nodes) => {
+    let x = new Array(nodes).fill(0);
+    let y = new Array(nodes).fill(0);
+    const ng = grid
+        .map(k => {
+            switch (k[0]){
+                case 'R':
+                    x[0] += k[1]
+                    break;
+                case 'L':
+                    x[0] -= k[1]
+                    break;
+                case 'U':
+                    y[0] += k[1]
+                    break;
+                case 'D':
+                    y[0] -= k[1]
+                    break;
+            }
+
+            const pos = [];
+            let b = true;
+            while (b){
+                b=false;
+                for(let i = 0; i<nodes-1; i++){
+                    if(Math.abs(x[i]-x[i+1]) > 1 || Math.abs(y[i]-y[i+1])>1) b=true;
+                    else continue;
+                    x[i+1] = x[i+1]-x[i] >= 1 ? x[i+1]-1 : x[i+1]-x[i] <= -1 ? x[i+1]+1 : x[i+1];
+                    y[i+1] = y[i+1]-y[i] >= 1 ? y[i+1]-1 : y[i+1]-y[i] <= -1 ? y[i+1]+1 : y[i+1];
+                    if(i==nodes-2) pos.push(x[i+1]+":"+y[i+1])
+                }
+            }
+            return pos;
+        }).flat();
+    ng.push("0-0");
+    return ng;
+}
+
+Array.prototype.remove_mltple = function() {return this
+    .filter((item,index) => this.indexOf(item) === index);
+}
 
 exports.run = (input) => {
     const grid = parse(input);
-    return Math.max(...calculate(grid, grid.trans()).map(x=>Math.max(...x)));
+	
+    return tocoo(grid, 10).remove_mltple().length;
 }
 
