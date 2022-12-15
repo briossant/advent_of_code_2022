fc2d1331b0c38e4de129b9081a2a39ce7ed0a8ea
Crosson Brieuc <crosson.brieuc@gmail.com>
2022-12-15 16:32:13 +0100

    d13 pt 2

--- d13/script2.js
+++ d13/script2.js
@@ -1,37 +1,36 @@
 
+const next = (l1,l2) => {
+    for(let i=0;i<l1.length;i++){
+        let k=0
+        if(l2.length <= i) return -1;
+
+        if(Array.isArray(l1[i])){
+            k = next(l1[i], Array.isArray(l2[i]) ? l2[i] : [l2[i]]);
+        }else if(Array.isArray(l2[i])){
+            k = next(Array.isArray(l1[i]) ? l1[i] : [l1[i]], l2[i]);
+        }else{
+            k = Math.sign(l2[i]-l1[i])
+        }          
+        if(k!==0){
+            return k;
+        }
+    }
+    return l2.length == l1.length ? 0 : 1;
+}
 
 const parse = (input) => input 
     .split('\n\n')
-    .map(x => x
-        .split('\n').map(x=>x.split(' '))
-    ).map(x => {return {
-        queue: x[1].slice(4).map(x=>parseInt(x.replace(/,/g, ""))),
-        opp: x[2][x[2].length-2],
-        strg: x[2][x[2].length-1],
-        modo: parseInt(x[3][x[3].length-1]),
-        test: (y) => y % parseInt(x[3][x[3].length-1])==0 ? parseInt(x[4][x[4].length-1]) : parseInt(x[5][x[5].length-1]),
-        insp: 0
-    }});
+    .map(x=>x
+        .split('\n')
+        .map(x=>eval(x))
+        .filter(x=>x!==undefined)
+    ).flat();
 
 
-const loop = (monkeys, iter) => {
-    const modo = monkeys.reduce((a,x)=>a*x.modo,1);
-    for(let i=0;i<iter;i++){
-        monkeys.forEach(x => {
-            x.queue.forEach(y=>{
-                strg = x.strg == 'old' ? y : parseInt(x.strg);
-                y = (x.opp == '*' ? y*strg : y+strg) % modo;
-                monkeys[x.test(y)].queue.push(y);
-                x.insp += 1;
-            });
-            x.queue = [];
-        });
-    }
-    return monkeys;
-}
-
-exports.run = (input) => loop(parse(input), 10000)
-    .map(x=>x.insp)
-    .sort((a,b)=>b-a)
-    .reduce((a,x,i)=>i<2 ? a*x : a,1);
-
+exports.run = (input) => {
+    let ls = parse(input)
+    ls.push([[2]]);
+    ls.push([[6]]);
+    ls = ls.sort((a,b)=>-next(a,b)).map(x=>x.toString());
+    return (1+ls.indexOf("2")) * (1+ls.indexOf("6"));
+};
